3940823741fc7aaa2d3e71c471aafad4a70a7b48
Eray Ates <72857634+erayates@users.noreply.github.com>
2022-08-08 03:53:33 +0300

    Added IP details (country,city etc.) with another API.

--- Get Your IP Address/script.js
+++ Get Your IP Address/script.js
@@ -1,13 +1,33 @@
 const siteTitle = document.querySelector('.site-title')
 const ipAddress = document.querySelector('.ip-address')
+const container = document.querySelector('.container')
 
 axios('https://api.ipify.org/?format=json').then(res => getIpAddress(res.data.ip))
-    
+
     
 
 const getIpAddress = (res) => {
     siteTitle.innerText = res
     ipAddress.innerText = res
+   axios(`https://ipinfo.io/${res}?token=5222404a7029a7`).then(resDetail => getIpDetails(resDetail.data))
+}
+
+const getIpDetails = (resDetail) => {
+
+    var pArray = []
+    for(let i = 0; i<6; i++){
+        pArray.push(document.createElement('p'))
+    }
+     pArray[0].innerHTML = `<strong>City:</strong> ${resDetail.city}`
+     pArray[1].innerHTML = `<strong>Country:</strong> ${resDetail.country}`
+     pArray[2].innerHTML = `<strong>Org:</strong> ${resDetail.org}`
+     pArray[3].innerHTML = `<strong>Postal:</strong> ${resDetail.postal}`
+     pArray[4].innerHTML = `<strong>Region:</strong> ${resDetail.region}`
+     pArray[5].innerHTML =`<strong>Timezone:</strong> ${resDetail.timezone}`
+
+    for(let i = 0; i<6; i++){
+         container.append(pArray[i])
+     }
 }
 
 
